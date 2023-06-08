e8d4d7663f4abc843a099a14b62fddee20361ae1
Ever <evertonxdoj@gmail.com>
2023-06-08 12:51:42 -0300

    starting to register commands

--- src/index.js
+++ src/index.js
@@ -1,16 +1,49 @@
-import { Client } from 'discord.js'
+import { Client, GatewayIntentBits, Routes } from 'discord.js'
+import { REST } from '@discordjs/rest'
 import { config } from 'dotenv'
 config() // get the enviroment(dotenv) variables
 
-// CLIENT is the bot; Intents is what the bot can do
-const CLIENT = new Client({
-    intents: [ 'Guilds', 'GuildMessages' ]
+// Intents is what the bot can do
+const BOT = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
 })
-const TOKEN = process.env.BOT_TOKEN
+const BOT_TOKEN = process.env.TOKEN
+const BOT_ID = process.env.CLIENT_ID
+const SERVER_ID = process.env.GUILD_ID
+const REST_ = new REST({ version: '10' }).setToken(BOT_TOKEN)
 
-CLIENT.on('ready', () => {
-    console.log('O rato está pronto!')
+BOT.on('ready', () => console.log(`O rato está pronto!`))
+
+BOT.on('messageCreate', (message) => {
+    console.log(message.content),
+    console.log(message.author.tag)
 })
 
+BOT.on('interactionCreate', (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+})
 
-CLIENT.login(TOKEN)
+async function main() {
+    const COMMANDS = [
+        {
+            name: 'ping',
+            description: 'Replies with pong!'
+        }
+    ]
+
+    try {
+        console.log('O rato está preparando os comandos...')
+        await REST_.put(Routes.applicationGuildCommands(BOT_ID, SERVER_ID), {
+            body: COMMANDS
+        })
+        BOT.login(BOT_TOKEN)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+main()
